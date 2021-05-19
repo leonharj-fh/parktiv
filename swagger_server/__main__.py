33c3da3ff51e0a48b85728b9458fddc1bbb9993e
leonharj-fh <84247579+leonharj-fh@users.noreply.github.com>
2021-05-19 18:49:52 +0200

    Update __main__.py
    added port cpmfog

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -9,7 +9,7 @@ def main():
 	app = connexion.FlaskApp(__name__, specification_dir='./swagger/', server="gevent")
 	app.app.json_encoder = encoder.JSONEncoder
 	app.add_api('swagger.yaml', arguments={'title': 'Parktiv'}, options=options, validate_responses=True)
-	app.run()
+	app.run(port=8080)
 
 
 if __name__ == '__main__':
