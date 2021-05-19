03679298d6377922a962b4095f0101e23428e028
leonharj-fh <84247579+leonharj-fh@users.noreply.github.com>
2021-05-19 18:08:05 +0200

    Update __main__.py
    Changed to production server

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -6,10 +6,10 @@ from swagger_server import encoder
 def main():
 	options = {"swagger_ui": False}
 	# app = connexion.FlaskApp(__name__, specification_dir='./swagger/', server='tornado')
-	app = connexion.FlaskApp(__name__, specification_dir='./swagger/') #, server="gevent")
+	app = connexion.FlaskApp(__name__, specification_dir='./swagger/', server="gevent")
 	app.app.json_encoder = encoder.JSONEncoder
 	app.add_api('swagger.yaml', arguments={'title': 'Parktiv'}, options=options, validate_responses=True)
-	app.run(port=8080)
+	app.run()
 
 
 if __name__ == '__main__':
